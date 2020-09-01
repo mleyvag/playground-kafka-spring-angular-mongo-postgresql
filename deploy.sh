f259cbd04fd109f68b414c78ff44faf89eca379c
Miguel Leyva <leyvaguadalupemiguel@gmail.com>
2020-08-31 23:54:53 -0500

    IP in enviroment shell

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!bin/sh
+export MY_IP=192.168.1.40
 cd kafka-producer && \
 mvn clean package && cd .. && \
 cd kafka-consumer-topic-one && \
@@ -7,4 +8,5 @@ cd kafka-consumer-topic-two && \
 mvn clean package && cd .. && \
 docker-compose down && \
 docker-compose up --build -d && \
-docker ps -an6
+docker ps -an6 && \
+docker logs -f consumer_topic_one
