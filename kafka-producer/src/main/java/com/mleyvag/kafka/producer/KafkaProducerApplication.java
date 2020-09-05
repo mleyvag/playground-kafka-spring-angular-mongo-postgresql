ebde97ea2b27d9bc8c7a09e10c150d32c60e0b5f
Miguel Leyva <leyvaguadalupemiguel@gmail.com>
2020-09-05 15:30:57 -0500

    FIX warnings

--- kafka-producer/src/main/java/com/mleyvag/kafka/producer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/mleyvag/kafka/producer/KafkaProducerApplication.java
@@ -1,10 +1,5 @@
 package com.mleyvag.kafka.producer;
 
-import com.mleyvag.kafka.producer.service.KafkaProducerService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
